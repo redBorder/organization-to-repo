6fbde25782f5b2aaa199baa5fb31baa7bdf61426
Miguel Alvarez <malvarez@redborder.com>
2024-04-22 17:01:20 +0100

    Add requirements.txt

--- __main__.py
+++ __main__.py
@@ -11,7 +11,7 @@
 
 
 import logging
-from env.load import load_environment_variables
+from env.load import *
 from parsers.arg import ArgParser
 from parsers.repo import RepoParser
 from organization.fetcher import OrgToRepos
@@ -20,8 +20,10 @@ from downloaders.assetdownloader import RpmDownloader
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-def download_rpms_for_organization(organization, rpm_downloader):
-    repos = OrgToRepos(GitHubAPI(), organization)
+if __name__ == '__main__':
+    args = ArgParser.parse_arguments()
+    repos = OrgToRepos(GitHubAPI(), args.organization)
+    rpm_downloader = RpmDownloader()
     for repo in repos.repos:
         repo_name = RepoParser.repo_url_to_repo_name(repo)
         assets = repos.get_latest_assets_release(repo_name)
@@ -30,15 +32,3 @@ def download_rpms_for_organization(organization, rpm_downloader):
                 logging.info(f"Downloading RPM from {asset}...")
                 rpm_downloader.download_and_move_rpm(asset)
                 logging.info(f"RPM downloaded and moved successfully.")
-
-def main():
-    try:
-        load_environment_variables()
-        args = ArgParser.parse_arguments()
-        rpm_downloader = RpmDownloader()
-        download_rpms_for_organization(args.organization, rpm_downloader)
-    except Exception as e:
-        logging.error(f"An error occurred: {e}")
-
-if __name__ == '__main__':
-    main()
