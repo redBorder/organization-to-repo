a4f4f8bbe242ab59b265f27147dfc79151aba085
Miguel Negrón <manegron@redborder.com>
2024-11-19 16:55:40 +0000

    Add load

---
+++ env/load.py
@@ -0,0 +1,16 @@
+###################################################################
+# This file is licensed under the Affero General Public License   #
+#                  Version 3 (AGPLv3)                             #
+#                                                                 #
+# You should have received a copy of the GNU Affero General       #
+# Public License along with this program. If not, see             #
+# <https://www.gnu.org/licenses/agpl-3.0.html>.                   #
+#                                                                 #
+# Author: malvarez@redborder.com                                  #
+###################################################################
+
+from dotenv import load_dotenv
+
+# Load env variables from .env file
+
+load_dotenv()
